1a7a7019fd31a860e8a50f84d5a83c588d9ef0d7
ishagupta-05 <49872185+ishagupta-05@users.noreply.github.com>
2019-10-23 09:37:02 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,21 @@
+public class even
+{
+public static void main (String args[])
+{
+
+int n=10;
+
+if(n%2==0)
+{
+System.out.println("the given number is even"+n);
+}
+else if(n%2!=0)
+{
+System.out.println("the given number is odd"+n);
+}
+else 
+{
+System.out.println("the given number is negative");
+}
+}
+}
